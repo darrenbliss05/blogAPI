b8f2d3f1c47f2211cc935e7809345519c1c64a82
bliss <darrenbliss05@gmail.com>
2016-12-22 14:10:23 -0500

    web.py actually supports sqlite. Converted to using the native sqlite database support instead of creating database code. The old database code remains to get the next post_id value during application startup. Not sure why dbadmin doesn't want to use autoincrement on the post_id field.

--- blogapi.py
+++ blogapi.py
@@ -2,6 +2,7 @@ import web
 import json
 import sqlite3
 import sys
+import socket
 
 urls = (
     '/posts', 'posts',
@@ -10,66 +11,72 @@ urls = (
 
 app = web.application(urls,globals())
 
-class posts:
-    def GET(self):
-       allposts = {'post_id':1,'title':'testing','body':'this is a test'}
-       web.header('Content-Type', 'application/json')
-       return json.dumps(allposts)
-
-class post:
-    def POST(self):
-       i = web.data()
-       return i + '\n'
-
 class blogdb:
     def __init__(self,dbfile):
         self.dbfile = dbfile
-        #self.dbconnect = sqlite3.connect(self.dbfile)
-        #self.dbcursor = self.dbconnect.cursor()
         self._opendb()
-        #self.retrieve_all_posts()
         self._set_db_id_counter()
-      #  self.db_id_counter = 0
 
     def _set_db_id_counter(self):
         self.db_id_counter = 1
         self.dbcursor.execute('select COALESCE(MAX(post_id)+1,0) from posts') 
         foo =  self.dbcursor.fetchone()[0]
-        print foo
-        print '\n'
         self.db_id_counter = int(foo)
-        print self.db_id_counter
       
-    def showtables(self):
-        self.dbcursor.execute('.tables;') 
-        print self.dbcursor.fetchall()
-        
+    def get_post_id_counter(self):
+        return(self.db_id_counter)
+
     def _opendb(self):
         self.dbconnect = sqlite3.connect(self.dbfile)
         self.dbconnect.text_factory = str
         self.dbcursor = self.dbconnect.cursor()
 
-    def _closedb(self):
+    def closedb(self):
         self.dbconnect.close()
         
-    def retrieve_all_posts(self):
-        self.dbcursor.execute('select post_id,title,body from posts') 
-        print self.dbcursor.fetchall()
 
-    def write_to_db(self):
-        title = "testing"
-        body = "This is a test"
-        self.dbcursor.execute('INSERT INTO posts VALUES (?,?,?)', (self.db_id_counter,title,body))
-        self.dbconnect.commit()
-        self.db_id_counter+=1
-        
+db = web.database(dbn="sqlite", db="blog.db")
+
+# sqlite db schema from DBA does not include autoincrementing
+# post_id. This is a workaround to get ths next available post_id
+# during startup.
+#
+dbinstance = blogdb('blog.db')
+post_id_counter = dbinstance.get_post_id_counter()
+dbinstance.closedb()  
+
+
+class posts:
+    def GET(self):
+       allposts = {'post_id':1,'title':'testing','body':'this is a test'}
+       web.header('Content-Type', 'application/json')
+       t = db.transaction()
+       results =  db.query("select * from posts")
+       return (json.dumps( [dict(ix) for ix in results] ))
+
+class post:
+    def POST(self):
+       global post_id_counter
+       data = json.loads(web.data())
+       t = db.transaction()
+       my_post_id = post_id_counter
+       post_id_counter += 1
+       try:
+          n = db.insert('posts', title=data["title"],body=data["body"],post_id=my_post_id)
+       except Exception, e:
+          emsg = 'ERROR: %s\n'  %(e)
+          print  emsg 
+       else:
+          t.commit()
+       return()
+
 
 if __name__ == "__main__":
-    dbinstance = blogdb('blog.db')
-    dbinstance.write_to_db()
-    dbinstance.write_to_db()
-    dbinstance.retrieve_all_posts()
-    sys.exit()
 #    Need to replace the hard-coded IP address with something that gets the IP adress of the current host
-    web.httpserver.runsimple(app.wsgifunc(), ("192.168.1.18",8080))
-    app.run()
+    try:
+       web.httpserver.runsimple(app.wsgifunc(), ("192.168.1.18",8080))
+    except Exception, e:
+        emsg = '%s' %(e)
+        print emsg
+        sys.exit(-1)
+
