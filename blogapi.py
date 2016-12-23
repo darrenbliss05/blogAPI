56e5b25bb2296769e0ad33480a83e7651cc0f3db
bliss <darrenbliss05@gmail.com>
2016-12-22 21:27:29 -0500

    Refactoring code switching from web.py to flask. POST is implemented correctly writing to the database. GET (posts) is currently just a stub.

--- blogapi.py
+++ blogapi.py
@@ -1,20 +1,21 @@
 
-import web
 import json
 import sqlite3
+from flask import request, jsonify
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 import sys
 import socket
 import os
+import time
 from daemonize import Daemonize
 import logging
-#import daemon
+from sqlalchemy import create_engine, MetaData, Table
 
-urls = (
-    '/posts', 'posts',
-    '/post' , 'post'
-)
 
-app = web.application(urls,globals())
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/bliss/web/proj/blogAPI/blog.db'
+db = SQLAlchemy(app)
 
 class blogdb:
     def __init__(self,dbfile):
@@ -40,39 +41,44 @@ class blogdb:
         self.dbconnect.close()
         
 
-db = web.database(dbn="sqlite", db="blog.db")
+#db = web.database(dbn="sqlite", db="blog.db")
 
 # sqlite db schema from DBA does not include autoincrementing
 # post_id. This is a workaround to get ths next available post_id
 # during startup.
 #
 dbinstance = blogdb('blog.db')
-post_id_counter = dbinstance.get_post_id_counter()
+post_id_counter = dbinstance.get_post_id_counter() - 1
 dbinstance.closedb()  
 
+class posts(db.Model):
+    post_id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(8192), unique=False)
+    title = db.Column(db.String(256), unique=False)
 
-class posts:
-    def GET(self):
-       web.header('Content-Type', 'application/json')
-       t = db.transaction()
-       results =  db.query("select * from posts")
-       return (json.dumps( [dict(ix) for ix in results] ))
+    def __init__(self, title, body,post_id):
+        self.title = title
+        self.body = body
+        self.post_id = post_id
+        print self.title, self.body, self.post_id
 
-class post:
-    def POST(self):
-       global post_id_counter
-       data = json.loads(web.data())
-       t = db.transaction()
-       my_post_id = post_id_counter
-       post_id_counter += 1
-       try:
-          n = db.insert('posts', title=data["title"],body=data["body"],post_id=my_post_id)
-       except Exception, e:
-          emsg = 'ERROR: %s\n'  %(e)
-          print  emsg 
-       else:
-          t.commit()
-       return()
+@app.route("/post",methods=['POST'])
+def POST():
+    global post_id_counter
+    if not request.headers['Content-Type'] == 'application/json':
+       return "MISSING JSON data"
+    inputdata = json.loads(json.dumps(request.json))
+    print inputdata['title']
+    print inputdata['body']
+    post_id_counter += 1
+    postblog = posts(inputdata['title'], inputdata['body'], post_id_counter)
+    db.session.add(postblog)
+    db.session.commit()
+    return "blog posted"
+
+@app.route("/posts")
+def GET():
+    return "Requesting all blogs"
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -82,8 +88,7 @@ def get_ip_address():
 #if __name__ == "__main__":
 def main():
     try:
-       #web.config.debug = False
-       web.httpserver.runsimple(app.wsgifunc(), (get_ip_address(),8080))
+        app.run(host=get_ip_address(), port=8080)
     except Exception, e:
         emsg = '%s' %(e)
         print emsg
@@ -91,13 +96,17 @@ def main():
 
 
 if __name__ == '__main__':
-       #main()
+        if (len(sys.argv) > 1):
+           if (sys.argv[1] == "--debug"):
+               main()
+               sys.exit()
+       
         myname=os.path.basename(sys.argv[0])
-        pidfile='/tmp/%s' % myname       # any name
+        pidfile='/tmp/%s.pid' % myname       # any name
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
         logger.propagate = False
-        fh = logging.FileHandler("/tmp/test.log", "w")
+        fh = logging.FileHandler("/tmp/blogsrcv.log", "w")
         fh.setLevel(logging.DEBUG)
         logger.addHandler(fh)
         keep_fds = [fh.stream.fileno()]
